a6ee4a148faf2da2c3108e37df861bc57ded01a1
Kristaps <kristaps1000@gmail.com>
2024-12-21 19:01:39 +0200

    helper funciton for asserting correct message

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -6,18 +6,19 @@ func TestHello(t *testing.T) {
 	t.Run("Saying hello to someone", func(t *testing.T) {
 		got := Hello("Kristap")
 		want := "Hello, Kristap!"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to \"World\"", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
