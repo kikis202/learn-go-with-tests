236613d51e518df9df8689266ee99491cfc4bb3a
Kristaps <kristaps1000@gmail.com>
2025-01-24 22:13:12 +0200

    Rectangle struct

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,20 +2,24 @@ package structs
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
+func TestRectangle(t *testing.T) {
+	rect := Rectangle{10.0, 10.0}
 
-	if got != want {
-		t.Errorf("got %2.f want %2.f", got, want)
-	}
-}
+	t.Run("perimeter", func(t *testing.T) {
+		got := Perimeter(rect)
+		want := 40.0
 
-func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
+		if got != want {
+			t.Errorf("got %2.f want %2.f", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("area", func(t *testing.T) {
+		got := Area(rect)
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
