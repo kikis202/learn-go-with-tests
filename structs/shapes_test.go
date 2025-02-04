1f4b6108c00ea9f5807434c65ff7a67a7b666eb9
Kristaps <kristaps1000@gmail.com>
2025-02-04 23:30:03 +0200

    Shape interface

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -26,24 +26,22 @@ func TestRectangle(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 
 }
