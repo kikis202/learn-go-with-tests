643bf8024ebf83cc47a6ecdcb54282d4582dc6f5
Kristaps <kristaps1000@gmail.com>
2025-02-04 23:23:36 +0200

    Working shape methods

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,11 +15,35 @@ func TestRectangle(t *testing.T) {
 	})
 
 	t.Run("area", func(t *testing.T) {
-		got := Area(rect)
+		got := rect.Area()
 		want := 100.0
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
+}
+
+func TestArea(t *testing.T) {
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
 }
