5669a8da0156a718dd1b69142e1a9d48b5c6ce1e
Kristaps <kristaps1000@gmail.com>
2025-06-21 17:17:45 +0300

    basic dict search

---
+++ maps/dictionary_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	t.Run("search", func(t *testing.T) {
+		key := "test"
+		value := "this is just a test"
+		dictionary := map[string]string{key: value}
+
+		got := Search(dictionary, key)
+		want := value
+
+		assertStrings(t, got, want)
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
