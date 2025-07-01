31159ae9dffae332df343c8a78c72196e5267a96
Kristaps <kristaps1000@gmail.com>
2025-07-01 18:30:43 +0300

    working tests

--- reflection/walk.go
+++ reflection/walk.go
@@ -3,7 +3,34 @@ package main
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	if val.Kind() == reflect.Slice {
+		for i := range val.Len() {
+			walk(val.Index(i).Interface(), fn)
+		}
+
+		return
+	}
+
+	for i := range val.NumField() {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -1,25 +1,89 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
 
-	want := "Kristaps"
-	var got []string
-
-	x := struct {
-		Name string
-	}{want}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name:          "Struct with one string field",
+			Input:         struct{ Name string }{"Kristaps"},
+			ExpectedCalls: []string{"Kristaps"},
+		},
+		{
+			Name: "Struct with three string fields",
+			Input: struct {
+				Name    string
+				City    string
+				Country string
+			}{"Kristaps", "Riga", "Latvia"},
+			ExpectedCalls: []string{"Kristaps", "Riga", "Latvia"},
+		},
+		{
+			Name: "Struct with non-string field",
+			Input: struct {
+				Name string
+				Age  int
+			}{"Kristaps", 23},
+			ExpectedCalls: []string{"Kristaps"},
+		},
+		{
+			Name: "Nested struct",
+			Input: Person{
+				Name: "Kristaps",
+				Profile: Profile{
+					City: "Riga",
+					Age:  23,
+				},
+			},
+			ExpectedCalls: []string{"Kristaps", "Riga"},
+		},
+		{
+			Name: "Pointers",
+			Input: &Person{
+				Name: "Kristaps",
+				Profile: Profile{
+					City: "Riga",
+					Age:  23,
+				},
+			},
+			ExpectedCalls: []string{"Kristaps", "Riga"},
+		},
+		{
+			Name:          "slice",
+			Input:         []Profile{{"Riga", 23}, {"Tukums", 11}},
+			ExpectedCalls: []string{"Riga", "Tukums"},
+		},
 	}
 
-	if got[0] != want {
-		t.Errorf("got %q, want %q", got[0], want)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, expected %v", got, test.ExpectedCalls)
+			}
+		})
 	}
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	City string
+	Age  int
 }
