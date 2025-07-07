cf8b2a391c60561a241d699c0ea2fcaa470dd1a1
Kristaps <kristaps1000@gmail.com>
2025-07-07 23:34:43 +0300

    basic error handling

--- blog/blogposts_test.go
+++ blog/blogposts_test.go
@@ -1,21 +1,46 @@
 package blogposts_test
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 
 	blogposts "github.com/kikis202/learn-go-with-tests/learnGoWithTests/blog"
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, I always fail")
+}
+
 func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
-	}
+	t.Run("reading FS with files should return corresponding posts", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte("hi")},
+			"hello-world2.md": {Data: []byte("hola")},
+		}
 
-	posts := blogposts.NewPostsFromFs(fs)
+		posts, err := blogposts.NewPostsFromFs(fs)
 
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(posts) != len(fs) {
+			t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+		}
+	})
+
+	t.Run("failing to open FS should return error", func(t *testing.T) {
+		fs := StubFailingFS{}
+
+		_, err := blogposts.NewPostsFromFs(fs)
+
+		if err == nil {
+			t.Error("Expected to get an error when file system fails")
+		}
+	})
 }
