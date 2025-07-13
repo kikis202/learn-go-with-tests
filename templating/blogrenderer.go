5ea49ee2deb7f1c1fa77b7b4fd2958ec715724d8
Kristaps <kristaps1000@gmail.com>
2025-07-14 01:18:28 +0300

    working rough post html

--- templating/blogrenderer.go
+++ templating/blogrenderer.go
@@ -8,6 +8,22 @@ import (
 )
 
 func Render(w io.Writer, post blogposts.Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", post.Title)
-	return err
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>\n", post.Title, post.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprint(w, "Tags: <ul>")
+	for _, tag := range post.Tags {
+		fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprint(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
