205a7165ef13032be9a64c8a85ae6419c70e7ef8
Kristaps <kristaps1000@gmail.com>
2025-07-14 20:49:04 +0300

    parsing templates just once

--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
@@ -10,21 +11,49 @@ import (
 )
 
 func TestRender(t *testing.T) {
-	aPost := blogposts.Post{
-		Title:       "hello world",
-		Body:        "This is a post",
-		Description: "This is a description",
-		Tags:        []string{"go", "tdd"},
+	var (
+		aPost = blogposts.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatalf("Didnt expect an error, but got %q", err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+
+	var (
+		aPost = blogposts.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for b.Loop() {
+		postRenderer.Render(io.Discard, aPost)
+	}
+
 }
