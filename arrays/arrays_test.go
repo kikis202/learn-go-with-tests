615d2c52e1e217f897ac04379d62e15108714e8c
Kristaps <kristaps1000@gmail.com>
2025-08-05 21:25:41 +0300

    more reduction usecases

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -21,6 +21,13 @@ func assertCorrectSum(t testing.TB, got, want int, numbers []int) {
 	}
 }
 
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2, 3, 4, 5}, []int{1, 2, 3})
 	want := []int{15, 6}
@@ -31,7 +38,6 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-
 	checkSums := func(t testing.TB, got, want []int) {
 		t.Helper()
 
@@ -50,5 +56,23 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{}, []int{1, 2, 3})
 		want := []int{0, 5}
 		checkSums(t, got, want)
+	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(acc, v int) int {
+			return acc * v
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(acc, v string) string {
+			return acc + v
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
 	})
 }
