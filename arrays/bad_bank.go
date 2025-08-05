10cb2c85e3ed5d18ebe321587067e5d26fc22e52
Kristaps <kristaps1000@gmail.com>
2025-08-05 22:13:45 +0300

    improved transaction handling

--- arrays/bad_bank.go
+++ arrays/bad_bank.go
@@ -1,9 +1,37 @@
 package arrays
 
+type Account struct {
+	Name    string
+	Balance float64
+}
+
 type Transaction struct {
 	From string
 	To   string
 	Sum  float64
+}
+
+func NewTransaction(a, b Account, sum float64) Transaction {
+	return Transaction{
+		From: a.Name,
+		To:   b.Name,
+		Sum:  sum,
+	}
+}
+
+func applyTransaction(a Account, t Transaction) Account {
+	if t.From == a.Name {
+		a.Balance -= t.Sum
+	}
+	if t.To == a.Name {
+		a.Balance += t.Sum
+	}
+	return a
+}
+
+func NewBalanceFor(a Account, t []Transaction) Account {
+	return Reduce(t, applyTransaction, a)
+
 }
 
 func BalanceFor(transactions []Transaction, name string) float64 {
