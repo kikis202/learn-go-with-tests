9f1fdb3318f8df17dd4195a1b9a69a9c396bbc1f
Kristaps <kristaps1000@gmail.com>
2025-08-10 22:36:32 +0300

    dockerfile for building and serving go package

--- accept_tests/cmd/httpserver/greeter_server_test.go
+++ accept_tests/cmd/httpserver/greeter_server_test.go
@@ -1,12 +1,39 @@
 package main_test
 
 import (
+	"context"
+	"os"
 	"testing"
 
+	"github.com/alecthomas/assert/v2"
 	"github.com/kikis202/learn-go-with-tests/go-specs-greet/specifications"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	driver := go_specs_greeter.Driver{BaseURL: "http://localhost:8080"}
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:        "../../.",
+			Dockerfile:     "./cmd/httpserver/Dockerfile",
+			BuildLogWriter: os.Stdout,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
+	driver := specifications.Driver{BaseURL: "http://localhost:8080"}
 	specifications.GreetSpecification(t, driver)
 }
